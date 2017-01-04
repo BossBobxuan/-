0bb7a84797795dee26cee8eb8bacb1b73e51c8bc
BossBobxuan <Bossxuan@bossxuandemacbook-pro.local>
2017-01-04 17:01:47 +0800

    Initial Commit

---
+++ 约玩平台/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  约玩平台
+//
+//  Created by Bossxuan on 17/1/4.
+//  Copyright © 2017年 Bossxuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
